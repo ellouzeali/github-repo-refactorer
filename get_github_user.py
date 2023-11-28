3adeb4c45b872fa0a1d7faa733a98caddbe8d5f6
ellouzeali <72167342+ellouzeali@users.noreply.github.com>
2023-11-28 15:49:35 +0100

    Search user in github organization

--- get_github_user.py
+++ get_github_user.py
@@ -1,9 +1,10 @@
 import getpass
 from github import Github
 
-def get_github_username(github_token, full_name):
+def get_github_username(github_token, full_name, organization_name):
     g = Github(github_token)
-    users = g.search_users(full_name + " in:fullname")
+    query = full_name + " in:fullname type:user org:" + organization_name
+    users = g.search_users(query)
     for user in users:
         return user.login
     return None
@@ -16,11 +17,12 @@ def main():
     github_token = getpass.getpass("Enter your GITHUB_TOKEN: ")
 
     users = ["Nassim Kebbani", "Thibault Deregnaucourt", "Ali ELLOUZE", "Julie Franel", "Olivier Lemaire"]
+    organization_name = "TotalEnergiesCode"
 
     for user in users:
         print("****************************************")
         print(f"User: {user}")
-        username = get_github_username(github_token, user)
+        username = get_github_username(github_token, user, organization_name)
         print(f"Username: {username}")
 
 
